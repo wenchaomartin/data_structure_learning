c38a06d9c9755652ab154521f884493463c856ac
martin <wenchaomartin@gmail.com>
2021-02-03 15:04:58 +0800

    ad .gitignore file

--- src/main/java/me/data/structure/learning/tree/node/TreeBuilder.java
+++ src/main/java/me/data/structure/learning/tree/node/TreeBuilder.java
@@ -7,74 +7,73 @@ import java.util.List;
 /**
  * Create by songwenchao on 2019-09-01
  */
-public  class TreeBuilder<V extends Comparable<V> > {
+public class TreeBuilder<V extends Comparable<V>> {
 
-  TreeNode build(V[] values){
-    TreeNode root = null;
-    for(int i=0;i<values.length;i++){
-      if(i==0){
-       root = newTree(null,values[0]);
-      }else{
-      root = newTree(root,values[i]);
-      }
+    TreeNode build(V[] values) {
+        TreeNode root = null;
+        for (int i = 0; i < values.length; i++) {
+            if (i == 0) {
+                root = newTree(null, values[0]);
+            } else {
+                root = newTree(root, values[i]);
+            }
+        }
+        return root;
     }
-    return root;
-  }
 
-   private  TreeNode newTree(TreeNode node, V object){
+    private TreeNode newTree(TreeNode node, V object) {
 
-    if(node == null){
-      node = new TreeNode(object);
+        if (node == null) {
+            node = new TreeNode(object);
+        } else if (node.getValue().compareTo(object) > 0) {
+            node.leftChild = newTree(node.leftChild, object);
+        } else {
+            node.rightChild = newTree(node.rightChild, object);
+        }
+        return node;
     }
-    else if(node.getValue().compareTo(object)>0){
-     node.leftChild = newTree(node.leftChild,object);
-    }else{
-      node.rightChild = newTree(node.rightChild,object);
-    }
-    return node;
-  }
 
-  public List<V> preTraversal(TreeNode<V> root) {
-    if (this == null) {
-      return new ArrayList<>();
-    } else {
-      List<V> list = new ArrayList<>();
-      list.add(root.getValue());
-      if (root.leftChild != null) {
-        list.addAll(preTraversal(root.leftChild));
-      }
-      if (root.rightChild != null) {
-        list.addAll(preTraversal(root.rightChild));
-      }
-      return list;
-    }
-  }
-
-  public List<V> preTravelsalWithoutRecusion (TreeNode<V> root) {
-
-    LinkedList<TreeNode<V>> stack = new LinkedList<>();
-    List<V> result = new ArrayList<>();
-
-    if(root == null){
-      return new ArrayList<>();
-    }else{
-        stack.push(root);
-        while(!stack.isEmpty()){
-
-          root = stack.pop();
-          result.add(root.getValue());
-
-          if(root.rightChild != null){
-            stack.push(root.rightChild);
-          }
-
-          if(root.leftChild!=null){
-            stack.push(root.leftChild);
-
-          }
+    public List<V> preTraversal(TreeNode<V> root) {
+        if (this == null) {
+            return new ArrayList<>();
+        } else {
+            List<V> list = new ArrayList<>();
+            list.add(root.getValue());
+            if (root.leftChild != null) {
+                list.addAll(preTraversal(root.leftChild));
+            }
+            if (root.rightChild != null) {
+                list.addAll(preTraversal(root.rightChild));
+            }
+            return list;
         }
     }
-    return result;
-  }
+
+    public List<V> preTravelSalWithoutRecursion(TreeNode<V> root) {
+
+        LinkedList<TreeNode<V>> stack = new LinkedList<>();
+        List<V> result = new ArrayList<>();
+
+        if (root == null) {
+            return new ArrayList<>();
+        } else {
+            stack.push(root);
+            while (!stack.isEmpty()) {
+
+                root = stack.pop();
+                result.add(root.getValue());
+
+                if (root.rightChild != null) {
+                    stack.push(root.rightChild);
+                }
+
+                if (root.leftChild != null) {
+                    stack.push(root.leftChild);
+
+                }
+            }
+        }
+        return result;
+    }
 
 }
--- src/main/java/me/data/structure/learning/tree/node/TreeNode.java
+++ src/main/java/me/data/structure/learning/tree/node/TreeNode.java
@@ -8,46 +8,43 @@ import java.util.List;
  */
 public class TreeNode<T extends Comparable<T>> implements Comparable<T> {
 
-  private T value;
+    private T value;
 
-   TreeNode<T> leftChild;
+    TreeNode<T> leftChild;
 
-   TreeNode<T> rightChild;
+    TreeNode<T> rightChild;
 
-  public TreeNode(T value) {
-    this.value = value;
-  }
+    public TreeNode(T value) {
+        this.value = value;
+    }
 
-  public T getValue() {
-    return value;
-  }
+    public T getValue() {
+        return value;
+    }
 
-  public void setValue(T value) {
-    this.value = value;
-  }
+    public void setValue(T value) {
+        this.value = value;
+    }
 
-  public TreeNode<T> getLeftChild() {
-    return leftChild;
-  }
+    public TreeNode<T> getLeftChild() {
+        return leftChild;
+    }
 
-  public void setLeftChild(TreeNode<T> leftChild) {
-    this.leftChild = leftChild;
-  }
+    public void setLeftChild(TreeNode<T> leftChild) {
+        this.leftChild = leftChild;
+    }
 
-  public TreeNode<T> getRightChild() {
-    return rightChild;
-  }
+    public TreeNode<T> getRightChild() {
+        return rightChild;
+    }
 
-  public void setRightChild(TreeNode<T> rightChild) {
-    this.rightChild = rightChild;
-  }
+    public void setRightChild(TreeNode<T> rightChild) {
+        this.rightChild = rightChild;
+    }
 
-  public int compareTo(T o) {
-    return this.value.compareTo(o);
-  }
-
-
-
+    public int compareTo(T o) {
+        return this.value.compareTo(o);
+    }
 
 
 }
